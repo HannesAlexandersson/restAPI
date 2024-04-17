b6573c0afd8a960df71f508f67a674a3d0f3d154
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-04-17 21:59:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const PORT = 3000;
+export const PORT = 10000;
 export const SECRET = "hdoshiwogdbw9273dhj"; //secret used for jwt token
 export const SALT = 10; //number of salt rounds used for password encryption
 
